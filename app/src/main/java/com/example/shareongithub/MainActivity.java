e36a87508a8a88e302da54b7a52f7382ee30d18a
Jack <jamaldonado@csumb.edu>
2024-04-20 17:03:10 -0700

    Edited xml files, edited MainActivity.java
    Error running program

--- app/src/main/java/com/example/shareongithub/MainActivity.java
+++ app/src/main/java/com/example/shareongithub/MainActivity.java
@@ -3,34 +3,60 @@ package com.example.shareongithub;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.shareongithub.databinding.ActivityMainBinding;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private static final String TAG = "DAC_GRADEXLOG";
+    String mCourse = "";
+    double mGrade = 0.0;
+    String mSemester = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button loginButton = findViewById(R.id.button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.CourseView.setMovementMethod( new ScrollingMovementMethod());
+        
+        binding.EnterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create an Intent to start the LoginActivity
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+                getInformationFromDisplay();
+                updateDisplay();
+
             }
         });
 
-        Button newUserButton = findViewById(R.id.button2);
-        newUserButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create an Intent to start the NewUserActivity
-                Intent intent = new Intent(MainActivity.this, NewUserActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
+
+    private void updateDisplay() {
+        String currentInfo = binding.CourseView.getText().toString();
+        Log.d(TAG , "current info: " + currentInfo);
+        String newDisplay = String.format(Locale.US , "Course:%s%nGrade:%.2f%nSemester:%s%n=-=-=-=%n%s" , mCourse , mGrade , mSemester , currentInfo);
+        binding.CourseView.setText(newDisplay);
+    }
+
+    private void getInformationFromDisplay() {
+        mCourse = binding.CourseInput.getText().toString();
+
+        try {
+            mGrade = Double.parseDouble(binding.GradeInput.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d("TAG" , "Error reading value from Grade edit text");
+        }
+
+        mSemester = binding.SemesterText.getText().toString();
     }
 }
