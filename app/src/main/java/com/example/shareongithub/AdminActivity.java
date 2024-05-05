f007c1c9141d38e565187be4277d35db00210ff4
Jesus Ramos <jesusri902@gmail.com>
2024-05-05 16:35:39 -0700

    admin correct implementation

--- app/src/main/java/com/example/shareongithub/AdminActivity.java
+++ app/src/main/java/com/example/shareongithub/AdminActivity.java
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AdminActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.admin); // Assuming your layout file is named admin.xml
+        setContentView(R.layout.admin); //correctly redirects to admin
     }
 }
